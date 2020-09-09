3577c1473423db59935bbd5279f32c5567df0691
Olha-dev <71031581+Olha-dev@users.noreply.github.com>
2020-09-09 20:49:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+  <title><em>Olha Sqvchenko 1395ae</em></title>
+  <meta charset="UTF-8">
+</head>
+<body>
+<p>09/09/2020</p>
+<h1><strong>Programming languages</strong></h1>
+<span class>HTML</span>
+<span style="color:green"></span>
+<div>CSS,JavaScript</div>
+<ul>
+<li>Java</li>
+<li>Python</li>
+<li>C++</li>
+</ul>
+<a href="http://htmlbook.ru/blog/shablony-html5" target="_blank">Press to explore HTML</a>
+<a href="http://htmlbook.ru/css" target="_blank">CSS Guidance</a>
+<a href="http://htmlbook.ru/faq/kak-razmestit-neskolko-kartinok-ryadom-po-gorizontali" target="_blank">Moving pictures</a>
+</body>
+</html>
